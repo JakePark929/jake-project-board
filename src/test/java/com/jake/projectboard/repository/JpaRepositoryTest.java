0559c97238e3db0daaf168889526f7e0682cc936
epike <epikeran@gmail.com>
2022-10-24 16:56:34 +0900

    #10 - jpa test 작성
    기본 crud + 연관관계 매핑과 cascading이 잘 동작하는지 보려고 작성
    테스트 데이터와 관련이 있기 때문에, 이대로는 실패한다.

---
+++ src/test/java/com/jake/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,96 @@
+package com.jake.projectboard.repository;
+
+import com.jake.projectboard.config.JpaConfig;
+import com.jake.projectboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+//@ActiveProfiles("testdb") // test용으로 db옵션 실행
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DisplayName("JPA 연결 테스트") // Junit5 기능
+@Import(JpaConfig.class) // JpaConfig 옵션 Auditing이 자동으로 안 켜짐
+@DataJpaTest
+class JpaRepositoryTest {
+    private final ArticleRepository articleRepository; // Autowired 필드 주입을 할 수 있지만, Junit5, Boot에서 Test에서도 사용가능(Junit4는 runwith)
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select test")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // given when then 패턴
+        // Given
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123);
+    }
+
+    @DisplayName("insert test")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        // given when then 패턴
+        // Given
+        long previousCount = articleRepository.count();
+
+        // When
+        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount+1);
+    }
+
+    @DisplayName("update test")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+        // given when then 패턴
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updateHashtag = "#springboot";
+        article.setHashtag(updateHashtag);
+
+        // When
+        Article savedArticle = articleRepository.saveAndFlush(article); // rollback될 예정인 update 동작은 생략될 수 있음, save 를 flush 해줘야 함
+
+        // Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag",updateHashtag);
+    }
+
+    @DisplayName("delete test")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        // given when then 패턴
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        // When
+        articleRepository.delete(article); // rollback될 예정인 update 동작은 생략될 수 있음, save 를 flush 해줘야 함
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount-1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+    }
+}
