a37916955659f06804e81a24d65d35e9580a5465
epike <epikeran@gmail.com>
2022-10-26 08:14:43 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로
    엔티티 클래스에 들어가는 요소이고,
    도메인과 직접 연관이 없는 요소이므로
    추출이 가능하다.
    
    @MappedSuperclass 이용하여 상속 방식으로 추출함
    그 외 @DateTimeFormat 요소 추가 및 jpa 옵션 개선

---
+++ src/main/java/com/jake/projectboard/domain/AuditingFields.java
@@ -0,0 +1,40 @@
+package com.jake.projectboard.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class) // Auditing 리스너도 배치가능
+@MappedSuperclass
+public class AuditingFields {
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) //iso 객체 클래스 or 패턴방법
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt; // 생성일자
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String createdBy; // 생성자
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy; // 수정자
+}
