c640403eb2bce8875afd1fd0cce1df46d4b9de2d
epike <epikeran@gmail.com>
2022-10-26 13:49:52 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 부가적인 옵션이 
    들어갈 수 있음을 확인했기 때문에
    적절히 테스트 메소드 수정

--- src/main/java/com/jake/projectboard/controller/ArticleController.java
+++ src/main/java/com/jake/projectboard/controller/ArticleController.java
@@ -1,11 +1,21 @@
 package com.jake.projectboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
     // 실제코드 에서는 Autowired 생략 가능
     // ctrl+shift+f12 전체화면
+    // ctrl+shift+f9 recompile
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
--- src/test/java/com/jake/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/jake/projectboard/controller/ArticleControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
@@ -20,7 +21,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+//    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -29,9 +30,10 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
+//                .andDo(print());
     }
 
     @Disabled("구현 중")
