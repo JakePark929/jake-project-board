7b28f1e3452fbb67e952e557ce8c684472740450
epike <epikeran@gmail.com>
2022-10-26 12:52:26 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어올지 테스트로 명세를 뽑는다.

---
+++ src/main/java/com/jake/projectboard/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package com.jake.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+    // 실제코드 에서는 Autowired 생략 가능
+    // ctrl+shift+f12 전체화면
+}
