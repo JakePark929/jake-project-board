59161ea6a5e44f0f8c6e8b4f846027b59538b70f
epike <epikeran@gmail.com>
2022-10-26 17:18:53 +0900

    #16 - 시큐리티 설정을 모든 요청에 인증이 열리도록 수정
    지금은 테스트 개발 단계고, 
    아직 인증 기능 구현 단계에 이르지 않았으므로, 
    모든 인증 설정을 오픈한다.
    그리고 폼 로그인을 활성화 해서 로그인 뷰가 그려지게끔 함.
    최신 스프링 부트 2.7의 시큐리티 설정 방법을 활용
    
    * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

---
+++ src/main/java/com/jake/projectboard/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.jake.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+// Spring Boot 2.7 부터의 변화
+// Auto-Configuration jetbrain youtrack-jira
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@EnableWebSecurity // 안넣어도됨 auto config
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+//                .authorizeRequests().antMatchers("/**").permitAll()
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // 전체 들어갈 수 있게 허용
+//                .formLogin();
+                .formLogin().and()
+                .build();
+//        return http.build();
+    }
+}
