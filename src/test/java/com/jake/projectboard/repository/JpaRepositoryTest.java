7aa3ac153010ca0cdaff6f72e4f515ced014988d
epike <epikeran@gmail.com>
2022-10-27 22:22:53 +0900

    #19 - 리팩토링: 사용 안하는 import 정리
    강의 중 optimize import 하지 않은 듯.
    정리함

--- src/test/java/com/jake/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/jake/projectboard/repository/JpaRepositoryTest.java
@@ -5,14 +5,12 @@ import com.jake.projectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 //@ActiveProfiles("testdb") // test용으로 db옵션 실행
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
